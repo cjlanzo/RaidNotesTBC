a88ff9f8bb1e53c5a8353c282a4e9fa4bd7d3c7d
Chris Lanzo <cjlanzo@ncsu.edu>
2022-02-22 02:12:27 -0500

    Working tree implementation that saves data in trash/boss per encounter

--- src/RaidNotesTBC.lua
+++ src/RaidNotesTBC.lua
@@ -4,10 +4,29 @@ local libDBIcon = _G.LibStub("LibDBIcon-1.0")
 local AceSerializer = _G.LibStub("AceSerializer-3.0")
 
 function RaidNotes:OnInitialize()
-	-- self:db = _G.LibStub("AceDB-3.0")
+	self.db = _G.LibStub("AceDB-3.0"):New("RaidNotesTBC")
 
 	self:Print("Welcome to RaidNotesTBC")
 	RaidNotes:Show_Journal()
 
 	-- self:DrawMinimapIcon()
+end
+
+function RaidNotes:SaveNotes(id, frameName, text)
+	if not self.db.char[id] then self.db.char[id] = {} end
+
+	self.db.char[id][frameName] = text
+end
+
+function RaidNotes:LoadNotes(key)
+	local t = {}
+	if self.db.char[key] then
+		t.trash = self.db.char[key]["Trash"] or ""
+		t.boss = self.db.char[key]["Boss"] or ""
+	else
+		t.trash = ""
+		t.boss = ""
+	end
+
+	return t
 end
---
+++ src/raids.lua
@@ -0,0 +1,74 @@
+local karazhan = {}
+karazhan["Attumen the Huntsman"] = 652
+karazhan["Moroes"]               = 653
+karazhan["Maiden of Virtue"]     = 654
+karazhan["Opera Hall"]           = 655
+karazhan["Nightbane"]            = 662
+karazhan["The Curator"]          = 656
+karazhan["Terestian Illhoof"]    = 657
+karazhan["Shade of Aran"]        = 658
+karazhan["Netherspite"]          = 659
+karazhan["Prince Malchezaar"]    = 661
+
+local gruulsLair = {}
+gruulsLair["High King Maulgar"]      = 649
+gruulsLair["Gruul the Dragonkiller"] = 650
+
+local magtheridonsLair = {}
+magtheridonsLair["Magtheridon"] = 651
+
+local ssc = {}
+ssc["The Lurker Below"]       = 624
+ssc["Hydross the Unstable"]   = 623
+ssc["Leotheras the Blind"]    = 625
+ssc["Fathom-Lord Karathress"] = 626
+ssc["Morogrim Tidewalker"]    = 627
+ssc["Lady Vashj"]             = 628
+
+local tk = {}
+tk["High Astromancer Solarian"] = 732
+tk["A'lar"]                     = 730
+tk["Void Reaver"]               = 731
+tk["Kael'thas Sunstrider"]      = 733
+
+local hyjal = {}
+hyjal["Rage Winterchill"] = 618
+hyjal["Anetheron"]        = 619
+hyjal["Kaz'rogal"]        = 620
+hyjal["Azgalor"]          = 621
+hyjal["Archimonde"]       = 622
+
+local bt = {}
+bt["High Warlord Naj'entus"] = 601
+bt["Supremus"]               = 602
+bt["Shade of Akama"]         = 603
+bt["Teron Gorefiend"]        = 604
+bt["Gurtogg Bloodboil"]      = 605
+bt["Reliquary of Souls"]     = 606
+bt["Mother Shahraz"]         = 607
+bt["The Illidari Council"]   = 608-- might not work
+bt["Illidan Stormrage"]      = 609
+
+
+local sunwell = {}
+sunwell["Kalecgos"]     = 724
+sunwell["Brutallus"]    = 725
+sunwell["Felmyst"]      = 726
+sunwell["Eredar Twins"] = 727 -- might not work
+sunwell["M'uru"]        = 728
+sunwell["Kil'jaeden"]   = 729
+
+local test = {}
+test["Rakoria"] = 649
+test["Kugnar"]  = 650
+
+raids = {}
+raids["Karazhan"]             = karazhan
+raids["Gruul's Lair"]         = gruulsLair
+raids["Magtheridon's Lair"]   = magtheridonsLair
+raids["Serpentshrine Cavern"] = ssc
+raids["Tempest Keep"]         = tk
+raids["Mount Hyjal"]          = hyjal -- test this
+raids["Black Temple"]         = bt -- test this
+raids["The Sunwell"]          = sunwell -- test this
+raids["Terokkar Forest"]      = test -- remove this
