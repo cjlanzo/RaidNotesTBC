9955fb5d0c77b41fc56feb5899b992945652e527
Chris Lanzo <cjlanzo92@gmail.com>
2022-06-23 13:06:08 -0400

    turning off test mode

--- src/constants.lua
+++ src/constants.lua
@@ -1,10 +1,10 @@
 ADDON_NAME = "RaidNotesTBC"
 
 -- set to true to enable debug mode
-DEBUG_MODE = true
+DEBUG_MODE = false
 
 -- set to true to enable test mode, must also enable debug_mode
-TEST_MODE = DEBUG_MODE and true
+TEST_MODE = DEBUG_MODE and false
 TEST_ZONE = "Sunwell Plateau"
 TEST_TARGET = "Lady Sacrolash"
 TEST_INSTANCE_INFO = {
